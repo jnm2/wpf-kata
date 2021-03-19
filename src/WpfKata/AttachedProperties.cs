c118aa1a923bf9d5438dc41d94a09a0fd72822a8
jnm2 <me@jnm2.com>
2021-03-19 18:42:16 -0400

    Create attached property boilerplate

---
+++ src/WpfKata/AttachedProperties.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+
+namespace WpfKata
+{
+    public static class AttachedProperties
+    {
+        public static readonly DependencyProperty SelectAllOnFocusProperty = DependencyProperty.RegisterAttached(
+            "SelectAllOnFocus",
+            typeof(bool),
+            typeof(AttachedProperties),
+            new PropertyMetadata(default(bool)));
+
+        /// <summary>Helper for getting <see cref="SelectAllOnFocusProperty"/> from <paramref name="element"/>.</summary>
+        /// <param name="element"><see cref="DependencyObject"/> to read <see cref="SelectAllOnFocusProperty"/> from.</param>
+        /// <returns><c>SelectAllOnFocus</c> property value.</returns>
+        [AttachedPropertyBrowsableForType(typeof(DependencyObject))]
+        public static bool GetSelectAllOnFocus(DependencyObject element)
+        {
+            return (bool)element.GetValue(SelectAllOnFocusProperty);
+        }
+
+        /// <summary>Helper for setting <see cref="SelectAllOnFocusProperty"/> on <paramref name="element"/>.</summary>
+        /// <param name="element"><see cref="DependencyObject"/> to set <see cref="SelectAllOnFocusProperty"/> on.</param>
+        /// <param name="value"><c>SelectAllOnFocus</c> property value.</param>
+        public static void SetSelectAllOnFocus(DependencyObject element, bool value)
+        {
+            element.SetValue(SelectAllOnFocusProperty, value);
+        }
+    }
+}
